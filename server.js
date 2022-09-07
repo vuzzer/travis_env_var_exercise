f71e8b2203e3025a3430c73d145ee2c6f07831be
samajeste <parfaitkouess@gmail.com>
2022-09-07 20:22:58 +0000

    setting of travis.yml file

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
+const FAVORITE_INSTRUCTOR = process.env.FAVORITE_INSTRUCTOR;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
 }
